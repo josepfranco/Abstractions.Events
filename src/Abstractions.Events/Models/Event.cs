0c886e5207ac6a50a63a4ad051211ebe8a810a90
José Franco <josepgvf@gmail.com>
2021-01-03 21:02:27 +0000

    chore(net5): updated to net5.0

--- src/Abstractions.Events/Models/Event.cs
+++ src/Abstractions.Events/Models/Event.cs
@@ -10,26 +10,26 @@ namespace Abstractions.Events.Models
         /// <summary>
         /// The unique identifier of this event
         /// </summary>
-        public string Name { get; set; }
-        
+        public string Name { get; set; } = string.Empty;
+
         /// <summary>
         /// The unique identifier of the aggregate this event belongs to
         /// </summary>
-        public string AggregateName { get; set; }
-        
+        public string AggregateName { get; set; } = string.Empty;
+
         /// <summary>
         /// The higher the version, the more recent this event is within the store
         /// </summary>
-        public ulong Version { get; set; }
-        
+        public ulong Version { get; set; } = default;
+
         /// <summary>
         /// The date & time this event was created at
         /// </summary>
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = default;
         
         /// <summary>
         /// The optional extra data this event may contain
         /// </summary>
-        public IEventPayload Payload { get; set; }
+        public IEventPayload? Payload { get; set; }
     }
 }
