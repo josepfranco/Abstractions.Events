d1992a0d160dc757d8b517fe372c82acac850eb5
José Franco <josepgvf@gmail.com>
2021-05-28 20:26:32 +0100

    feat(model): updated model and added factory specification

--- src/Abstractions.Events/IEventPayload.cs
+++
@@ -1,7 +0,0 @@
-namespace Abstractions.Events
-{
-    /// <summary>
-    /// Represents a payload data object to be passed along an event
-    /// </summary>
-    public interface IEventPayload {}
-}
